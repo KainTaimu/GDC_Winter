06d35b61a0f1bdde05a62c59ea92bdfe46d602b7
Erwin Panergo <erwin.panergo1@gmail.com>
2025-12-24 08:27:50 -0700

    add: crosshair

--- src/Game.cs
+++ src/Game.cs
@@ -6,4 +6,12 @@ public partial class Game : Node
     {
         GetNode("/root/DebugMenu").Set("style", 2);
     }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsPhysicalKeyPressed(Key.Quoteleft))
+        {
+            GetTree().Quit();
+        }
+    }
 }
