497a33834ca4521abee396b5fc8d4ff1c63333e3
Erwin Panergo <erwin.panergo1@gmail.com>
2025-12-25 18:26:28 -0700

    add: snow

--- src/Player/StateParry.cs
+++ src/Player/StateParry.cs
@@ -4,48 +4,48 @@ namespace Game.Players;
 
 public partial class StateParry : State
 {
-    [Export]
-    private Area2D _parryArea;
+	[Export]
+	private Area2D _parryArea;
 
-    public override void _Process(double delta) { }
+	public override void _Process(double delta) { }
 
-    public override void Enter()
-    {
-        var overlapping = _parryArea.GetOverlappingAreas();
-        foreach (var area in overlapping)
-        {
-            OnProjectileEntered(area);
-        }
+	public override void Enter()
+	{
+		var overlapping = _parryArea.GetOverlappingAreas();
+		foreach (var area in overlapping)
+		{
+			OnProjectileEntered(area);
+		}
 
-        MovementController.ChangeState<StateGrounded>();
-    }
+		MovementController.ChangeState<StateGrounded>();
+	}
 
-    public override void Exit() { }
+	public override void Exit() { }
 
-    private void OnProjectileEntered(Area2D area)
-    {
-        switch (area)
-        {
-            case ProjectileBullet bullet:
+	private void OnProjectileEntered(Area2D area)
+	{
+		switch (area)
+		{
+			case ProjectileBullet bullet:
 
-                // Reflect
-                bullet.GlobalRotation *= -1;
-                bullet.Velocity = 1200;
-                break;
-            default:
-                break;
-        }
-    }
+				// Reflect
+				bullet.GlobalRotation *= -1;
+				bullet.Velocity = 1200;
+				break;
+			default:
+				break;
+		}
+	}
 
-    private float GetAngleFromPlayer(Vector2 point)
-    {
-        var viewport = GetViewport();
-        var player = GameWorld.Instance.MainPlayer;
-        var playerPos =
-            player.GlobalPosition * viewport.GetCamera2D().GetCanvasTransform().AffineInverse();
-        var crosshairPos = point * viewport.GetScreenTransform();
+	private float GetAngleFromPlayer(Vector2 point)
+	{
+		var viewport = GetViewport();
+		var player = GameWorld.Instance.MainPlayer;
+		var playerPos =
+			player.GlobalPosition * viewport.GetCamera2D().GetCanvasTransform().AffineInverse();
+		var crosshairPos = point * viewport.GetScreenTransform();
 
-        var angle = playerPos.AngleToPoint(crosshairPos);
-        return angle;
-    }
+		var angle = playerPos.AngleToPoint(crosshairPos);
+		return angle;
+	}
 }
