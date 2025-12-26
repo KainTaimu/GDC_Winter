1351babb4189dd9e3e805106a41afb02bac0ce47
Erwin Panergo <erwin.panergo1@gmail.com>
2025-12-25 22:01:14 -0700

    .

--- src/Player/StateDying.cs
+++ src/Player/StateDying.cs
@@ -2,8 +2,18 @@ namespace Game.Players;
 
 public partial class StateDying : State
 {
+    [Export]
+    private AnimatedSprite2D _sprite;
+
     public override void Enter()
     {
+        // BUG: WTF
+        // B
+        _sprite.Position = new Vector2(59f, -48f);
+        _sprite.Scale = new Vector2(4.9f, 4.8f);
+
+        _sprite.Animation = "dying";
+
         GameWorld.Instance.CurrentLevel.ScrollingBackground.Stop();
         MovementController.Lock(this);
         GameWorld.Instance.EmitSignal(GameWorld.SignalName.OnPlayerDeath);
